e48e1371e14cda063c487183ebfb3d8354e3f043
Tuyet Mai Catherine Phan <tuyetmai3000@gmail.com>
2015-03-17 18:27:22 -0400

    Create db.pl
    Rules to write

---
+++ db.pl
@@ -0,0 +1,18 @@
+is_loop(Event, Guard).
+all_loops(Set).
+is_edge(Event, Guard).
+size(Length).
+is_link(Event, Guard).
+all_superstates(Set).
+ancestor(Ancestor, Descendant).
+inherits_transitions(State, List).
+all_states(L).
+all_init_states(L).
+get_starting_state(State).
+state_is_reflexive(State).
+graph_is_reflexive.
+get_guards(Ret).
+get_event(Ret).
+get_actions(Ret).
+get_only_guarded(Ret).
+legal_events_of(State, L).
