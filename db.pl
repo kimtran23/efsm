53e13eeb92e86e2b32338e9ec70e9c236d10fdd5
Tuyet Mai Catherine Phan <tuyetmai3000@gmail.com>
2015-03-18 15:48:17 -0400

    Update db.pl
    Facts for part I, overall EFSM

--- db.pl
+++ db.pl
@@ -6,10 +6,20 @@ state(monitoring).
 state(error_diagnosis).
 state(safe_shutdown).
 %%start(dormant).
-%%final(dormant).
 
 %% transition(SourceState, DestinationSate, Event, Guard, Action).
-
+transition(dormant, init, start, no_guard, no_action). 
+transition(init, idle, init_ok, 'drivers loaded', no_action).
+transition(init, error_diagnosis, init_crash, no_guard, ['information logged', init_err_msg]).
+transition(idle, monitoring, begin_monitoring, no_guard, no_action).
+transition(idle, error_diagnosis, idle_crash, no_guard, idle_err_msg).
+transition(monitoring, error_diagnosis, monitor_crash, no_guard, moni_err_msg). 
+transition(error_diagnosis, idle, idle_rescue, no_guard, no_action).
+transition(error_diagnosis, monitoring, moni_rescue, no_guard, no_action).
+transition(error_diagnosis, safe_shutdown, shutdown, 'retry >= 3', no_action).
+transition(error_diagnosis, init, [retry_init, 'incr retry by 1'], 'retry < 3', no_action). 
+transition(safe_shutdown, dormant, sleep, no_guard, no_action).
+transition(dormant, exit, kill, no_guard, no_action).
 
 %% Part II: Refine init
 
